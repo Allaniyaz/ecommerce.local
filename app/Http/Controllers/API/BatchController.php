091336cd1d889124fc689166cb2595d5998a998d
Allaniyaz <a_aleke@inbox.ru>
2025-03-11 02:34:42 +0500

    realized getting products & calc profits

--- app/Http/Controllers/API/BatchController.php
+++ app/Http/Controllers/API/BatchController.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\APIController;
-use App\Models\User;
-use App\Models\Client;
+use App\Models\OrderProduct;
+use App\Models\Batch;
+use App\Models\BatchProduct;
 use App\Models\Product;
 use App\Http\Resources\ProductResource;
 use Illuminate\Http\Request;
@@ -20,7 +21,8 @@ class BatchController extends APIController
 
     public function index()
     {
-
+        $batches = Batch::with(['products'])->get();
+        return $this->successResponse($batches);
     }
 
     public function store(Request $request)
@@ -37,20 +39,62 @@ class BatchController extends APIController
             return $this->errorResponse($validator->errors());
         }
 
-        ///////
-        // $data['customer_id'] = $request->user()->customer->id;
-        // $data['registration_number'] = strtoupper($data['registration_number']);
-        // $data['techpassport'] = strtoupper($data['techpassport_series']) . $data['techpassport_number'];
+        // validating products
+        $err = [];
+        foreach($data['products'] as $product) {
 
+            $validator2 = Validator::make($product, [
+                'id' => 'required|integer|exists:products,id',
+                'name' => 'nullable',
+                'qty' => 'required|integer',
+                'price' => 'required|integer',
+            ]);
 
-        if ($garage = Garage::create($data)) {
-            $garage->name = $garage->model->name;
-            $garage->save();
-            return $this->successResponse(new GarageResource($garage), 'Record created.');
+            if ($validator2->fails()) {
+                $err[] = $validator2->errors();
+            }
         }
 
-        return $this->errorResponse('Error by saving data.');
+        if (!empty($err)) {
+            return $this->errorResponse($err);
+        }
+        //
 
+
+        $batch = new Batch;
+        $batch->provider_id = $data['provider_id'];
+        $batch->storage_id = $data['storage_id'];
+        $batch->save();
+
+        foreach ($data['products'] as $prod) {
+            $batch->products()->attach($prod['id'], [
+                    'qty' => $prod['qty'],
+                    'price' => $prod['price'],
+                    'remain_qty' => $prod['qty'],
+                    'storage_id' => $data['storage_id']
+            ]);
+        }
+
+        return $this->successResponse($batch);
+    }
+
+    public function profit(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => 'required|integer|exists:batches,id'
+        ]);
+
+        if ($validator->fails()) {
+            return $this->errorResponse($validator->errors());
+        }
+
+        $batch_products = BatchProduct::where('batch_id', $request->id)->get();
+        $order_products = OrderProduct::where('batch_id', $request->id)->get();
+
+        $payed_total = $batch_products->pluck('payed')->sum();
+        $sold_total = $order_products->pluck('sold')->sum();
+
+        return $this->successResponse($sold_total - $payed_total);
     }
 
 }
